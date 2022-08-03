59db7252495ee8c56f0f9bb8bcab85abd30377e0
Igor Tyan <tyanigor89@gmail.com>
2022-08-03 15:24:30 +0300

    added findShort function

--- homework_7/js/script.js
+++ homework_7/js/script.js
@@ -6,3 +6,11 @@ const sumTo = a => a > 1 ? a + sumTo(--a) : a;
 console.log(sumTo(1));
 console.log(sumTo(2));
 console.log(sumTo(3));
+
+// 2
+let findShort = str => str.split(" ").sort((a, b) => a.length - b.length)[0];
+
+console.log(findShort("Lorem ipsum dolor sit amet"));
+console.log(findShort("Hello world"));
+console.log(findShort("Hi"));
+console.log(findShort("She is David's sister"));
