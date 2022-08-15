7da6275b3706204075dccba4bf124e93aea4691f
Igor Tyan <tyanigor89@gmail.com>
2022-08-15 15:21:25 +0300

    added Employee class

--- homework_10/js/script.js
+++ homework_10/js/script.js
@@ -1,1 +1,32 @@
 "use strict";
+
+class Employee {
+
+  static vacationDaysPerYear = 18;
+
+  constructor(firstName, lastName, age, jobPosition, salary) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+    this.jobPosition = jobPosition;
+    this.salary = salary;
+  }
+
+  get fullName() {
+    return `${this.firstName} ${this.lastName}`;
+  }
+
+  calcAnnualSallary() {
+    return this.salary * 12;
+  }
+
+}
+
+const empMike = new Employee("Mike", "Tyson", 55, "Boxer", 50000);
+console.log(empMike.fullName);
+console.log(empMike.calcAnnualSallary());
+
+const empJohn = new Employee("John", "Constantine", 38, "Exorcist", 0);
+console.log(empJohn.fullName);
+console.log(empJohn.calcAnnualSallary());
+
