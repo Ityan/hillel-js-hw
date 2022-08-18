55d45d144a16ae51e4f3677694c01e97b3cfa228
Igor Tyan <tyanigor89@gmail.com>
2022-08-18 18:03:47 +0300

    addressed comment

--- homework_11/js/script.js
+++ homework_11/js/script.js
@@ -1,7 +1,9 @@
 "use strict";
 
-function isWeekend(dateStr) {
-  const dayOfWeek = new Date(dateStr).getDay();
+function isWeekendInCurrentYear(dateStr) {
+  const date = new Date(dateStr);
+  date.setFullYear(new Date().getFullYear());
+  const dayOfWeek = date.getDay();
   return dayOfWeek === 6 || dayOfWeek === 0;
 }
 
@@ -33,7 +35,7 @@ class Employee extends Person {
   }
 
   celebrate() {
-    if (isWeekend(this.birthDayDate)) {
+    if (isWeekendInCurrentYear(this.birthDayDate)) {
       return super.celebrate();
     }
     return "Happy Birthday, but I need to work";
