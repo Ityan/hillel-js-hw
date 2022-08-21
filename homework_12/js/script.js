6eee94231e98743f0d1fb0961f4c797ce3934ed2
Igor Tyan <tyanigor89@gmail.com>
2022-08-21 11:32:23 +0300

    added promises

--- homework_12/js/script.js
+++ homework_12/js/script.js
@@ -1,2 +1,37 @@
 "use strict";
 
+// create promises
+const successPromise = new Promise(function (resolve, reject) {
+  setTimeout(() => resolve("done"), 5000);
+});
+
+const failPromise = new Promise(function (resolve, reject) {
+  setTimeout(() => reject(new Error("Some err msg")), 5000);
+});
+
+// then/catch
+function logPromise(promise) {
+  promise
+    .then(reslove => console.log(reslove))
+    .catch(err => console.error(err.message))
+    .finally(() => console.log("Promise was finished!"));
+}
+
+logPromise(successPromise);
+logPromise(failPromise);
+
+// async/await
+async function logPromiseFn(promise) {
+  try {
+    const result = await promise;
+    console.log(result);
+  } catch (e) {
+    console.error(e.message);
+  } finally {
+    console.log("Promise was finished!")
+  }
+}
+
+logPromiseFn(successPromise);
+logPromiseFn(failPromise);
+
