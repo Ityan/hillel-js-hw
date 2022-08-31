7b428def76a48eae34eca2156be463be47ac84f1
Igor Tyan <tyanigor89@gmail.com>
2022-08-31 17:55:07 +0300

    fetched and filtered users

--- homework_13/js/script.js
+++ homework_13/js/script.js
@@ -1,2 +1,6 @@
 "use strict";
 
+const users = fetch('https://jsonplaceholder.typicode.com/users')
+  .then(response => response.json())
+  .then(data => data.filter(usr => usr.company?.name === 'Johns Group').forEach(usr => console.log(usr)))
+  .catch(err => console.error(err.message));
