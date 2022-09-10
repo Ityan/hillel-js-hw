4c04de5b8598be828e260b536effe93b0397a32a
Igor Tyan <tyanigor89@gmail.com>
2022-09-10 11:15:44 +0300

    init homework 15

---
+++ homework_16/js/script.js
@@ -0,0 +1,16 @@
+"use strict";
+
+const STYLE = "album_title";
+const albumsNode = document.getElementById("albums");
+
+function addListItem(listNode, styleClass, title) {
+  const itemNode = document.createElement("li");
+  itemNode.innerHTML = title;
+  itemNode.classList.add(styleClass);
+  listNode.append(itemNode);
+}
+
+fetch("https://jsonplaceholder.typicode.com/albums")
+  .then(response => response.json())
+  .then(data => data.forEach(item => addListItem(albumsNode, STYLE, item.title)))
+  .catch(err => console.error(err.message));
