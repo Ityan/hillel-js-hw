2bf75982f50437184f6570e6b7c6a44bfa7119cb
Igor Tyan <tyanigor89@gmail.com>
2022-09-10 12:41:27 +0300

    implemented item deletion

--- homework_16/js/script.js
+++ homework_16/js/script.js
@@ -1,16 +1,29 @@
 "use strict";
 
-const STYLE = "album_title";
+const ITEM_STYLE = "album-title";
+const BTN_STYLE = "delete-btn";
 const albumsNode = document.getElementById("albums");
 
-function addListItem(listNode, styleClass, title) {
+albumsNode.addEventListener("click", (event) => {
+  if (event.target.className === BTN_STYLE) {
+    event.target.parentNode.remove();
+  }
+});
+
+function addListItem(listNode, title) {
   const itemNode = document.createElement("li");
   itemNode.innerHTML = title;
-  itemNode.classList.add(styleClass);
+  itemNode.classList.add(ITEM_STYLE);
+
+  const btnNode = document.createElement("button");
+  btnNode.classList.add(BTN_STYLE);
+  btnNode.innerHTML = "X";
+  itemNode.prepend(btnNode);
+
   listNode.append(itemNode);
 }
 
 fetch("https://jsonplaceholder.typicode.com/albums")
   .then(response => response.json())
-  .then(data => data.forEach(item => addListItem(albumsNode, STYLE, item.title)))
+  .then(data => data.forEach(item => addListItem(albumsNode, item.title)))
   .catch(err => console.error(err.message));
