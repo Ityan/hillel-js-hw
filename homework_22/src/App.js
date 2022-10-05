ce6572110ef9c36f8b3aa77956b901c989b674b3
Igor Tyan <tyanigor89@gmail.com>
2022-10-05 09:27:16 +0300

    init homework 22

---
+++ homework_22/src/App.js
@@ -0,0 +1,12 @@
+import Title from "./components/Title";
+
+function App() {
+  return (
+    <div>
+      <Title name="Products" type="bold" />
+      <Title name="Employees" type="normal" />
+    </div>
+  );
+}
+
+export default App;
---
+++ homework_22/src/components/Title.js
@@ -0,0 +1,7 @@
+function Title({ name, type }) {
+  return (
+    <h2 style={{ fontWeight: type }}>{name}</h2>
+  );
+}
+
+export default Title;
