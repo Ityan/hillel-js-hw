ec21b11250a1eb26d4d9dd1e060e1c64b2e58143
Ihor Tian <tyanigor89@gmail.com>
2022-10-27 13:45:27 +0300

    added button

--- homework_26/src/App.js
+++ homework_26/src/App.js
@@ -1,8 +1,22 @@
+import styled from "styled-components";
+
+import Button from "./components/Button";
+
+const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 40px;
+`;
+
 function App() {
   return (
-    <>
-    
-    </>
+    <Wrapper>
+      <Button color="primary">Primary button</Button>
+      <Button color="primary" size="large">Primary large button</Button>
+      <Button color="secondary">Secondary button</Button>
+      <Button color="primary" disabled>Disabled button</Button>
+    </Wrapper>
   );
 }
 
---
+++ homework_26/src/components/Button.js
@@ -0,0 +1,88 @@
+import PropTypes from "prop-types";
+import styled from "styled-components";
+
+import NunitoSansWoff from "../assets/fonts/NunitoSans-Regular.woff";
+import NunitoSansWoff2 from "../assets/fonts/NunitoSans-Regular.woff2";
+
+const StyledButtonBase = styled.button`
+  @font-face {
+    font-family: 'Nunito Sans';
+    src: local('Nunito Sans'), local('NunitoSans'),
+    url(${NunitoSansWoff2}) format('woff2'),
+    url(${NunitoSansWoff}) format('woff');
+    font-weight: 300;
+    font-style: normal;
+  }
+
+  padding-right: 20px;
+  padding-left: 20px;
+  height: 48px;
+  border-radius: 4px;
+  border: 1px solid transparent;
+
+  font-family: 'Nunito Sans';
+  font-size: ${props => props.size === "large" ? "18px" : "16px"};
+
+  :disabled {
+    border-color: #7c8db0;
+    background: white;
+    color: #7C8DB0;
+  }
+`;
+
+const StyledPrimaryButton = styled(StyledButtonBase)`
+  background: #605dec;
+  color: #FAFAFA;
+
+  :hover {
+    background: #1513a0;
+  }
+
+  :disabled {
+    background: rgba(203, 212, 230, 0.3);
+  }
+`;
+
+const StyledSecondaryButton = styled(StyledButtonBase)`
+  background: white;
+  border-color: #605dec;
+  color: #605dec;
+
+  :hover {
+    background: #e9e8fc;
+  }
+
+  :disabled {
+    background: white;
+  }
+`;
+
+const Button = ({ onClick, color, disabled, children, size }) => {
+  const buttonProps = {
+    disabled,
+    onClick,
+    children,
+    size
+  };
+
+  if (color === "secondary") {
+    return <StyledSecondaryButton {...buttonProps} />;
+  }
+
+  return <StyledPrimaryButton {...buttonProps} />;
+};
+
+Button.propTypes = {
+  onClick: PropTypes.func,
+  color: PropTypes.oneOf(["primary", "secondary"]).isRequired,
+  size: PropTypes.oneOf(["large", "medium"]),
+  disabled: PropTypes.bool,
+  children: PropTypes.node.isRequired
+};
+
+Button.defaultProps = {
+  size: "medium",
+  disabled: false
+};
+
+export default Button;
